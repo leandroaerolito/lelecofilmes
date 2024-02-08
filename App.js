f0ccf8dd710b95c7fee2f498e0457e9b36614bc4
Leandro Rocha <leandro.aerolito@gmail.com>
2024-02-08 10:53:28 -0300

    criação de estilos iniciais para as sessões

--- App.js
+++ App.js
@@ -1,16 +1,70 @@
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <View>
-          <Text>App</Text>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.viewLogo}>
+          <Text>Leleco Filmes</Text>
+        </View>
+
+        <View style={estilos.viewBotoes}>
+          <Button title="Pesquisar" />
+          <Button title="Favoritos" />
+        </View>
+
+        <View style={estilos.viewRodape}>
+          <Button title="Privacidade" />
+          <Button title="Sobre" />
         </View>
       </SafeAreaView>
     </>
   );
 }
 
-const estilos = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "yellow",
+    flex: 1,
+  },
+
+  viewLogo: {
+    backgroundColor: "purple",
+    flex: 4,
+    marginLeft: 40,
+    marginRight: 40,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    width: "80%",
+  },
+
+  viewBotoes: {
+    backgroundColor: "grey",
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginLeft: 40,
+    marginRight: 40,
+    alignItems: "flex-start",
+    width: "80%",
+  },
+
+  viewRodape: {
+    flex: 0.5,
+    backgroundColor: "red",
+    marginRight: 40,
+    marginLeft: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    width: "80%",
+  },
+});
