a61892202924e5a8b8af06db0883a8e298e2c0b5
Leandro Rocha <leandro.aerolito@gmail.com>
2024-02-19 12:00:59 -0300

    Formatação da tela "Privacidade"

--- src/screens/Sobre.js
+++ src/screens/Sobre.js
@@ -7,39 +7,47 @@ import {
   View,
   Image,
   Pressable,
+  Linking,
+  ScrollView,
 } from "react-native";
 import SafeContainer from "../components/SafeContainer";
 import logomoviedb from "../../assets/images/logo-tmdb.png";
-impor;
 
 export default function Sobre() {
   return (
     <SafeContainer>
       <View style={estilos.subContainer}>
-        <Text style={estilos.subtitulo}>Sobre o app Leleco Filmes</Text>
-        <Text style={estilos.texto}>
-          O <Text style={estilos.nomeApp}>Leleco Filmes</Text> é um aplicativo
-          com a finalidade de permitir a busca por informações sobre filmes
-          existentes na base de dados pública disponibilizada pelo site The
-          Movie Database (TMDb).
-        </Text>
-        <View>
-          <Pressable>
-            <Image style={estilos.logoMovie} source={logomoviedb}></Image>
-          </Pressable>
-        </View>
+        <ScrollView>
+          <Text style={estilos.subtitulo}>Sobre o app Leleco Filmes</Text>
+          <Text style={estilos.texto}>
+            O <Text style={estilos.nomeApp}>Leleco Filmes</Text> é um aplicativo
+            com a finalidade de permitir a busca por informações sobre filmes
+            existentes na base de dados pública disponibilizada pelo site The
+            Movie Database (TMDb).
+          </Text>
 
-        <Text style={estilos.texto}>
-          Ao localizar um filme, o usuário pode visualizar informações como
-          título, data de lançamento, nota média de avaliação e uma breve
-          descrição sobre o filme e, caso queira, salvar estas informações em
-          uma lista no próprio aplicativo para visualização posterior.
-        </Text>
-        <Text style={estilos.texto}>
-          O aplicativo poderá receber novos recursos conforme o feedback e
-          demanda dos usuários.
-        </Text>
-        <Text style={estilos.texto}>LELECO FILMES &copy; 2024</Text>
+          <View>
+            <Pressable
+              onPress={() =>
+                Linking.openURL("https://www.themoviedb.org/?language=pt-BR")
+              }
+            >
+              <Image style={estilos.logoMovie} source={logomoviedb}></Image>
+            </Pressable>
+          </View>
+
+          <Text style={estilos.texto}>
+            Ao localizar um filme, o usuário pode visualizar informações como
+            título, data de lançamento, nota média de avaliação e uma breve
+            descrição sobre o filme e, caso queira, salvar estas informações em
+            uma lista no próprio aplicativo para visualização posterior.
+          </Text>
+          <Text style={estilos.texto}>
+            O aplicativo poderá receber novos recursos conforme o feedback e
+            demanda dos usuários.
+          </Text>
+          <Text style={estilos.texto}>LELECO FILMES &copy; 2024</Text>
+        </ScrollView>
       </View>
     </SafeContainer>
   );
@@ -58,6 +66,7 @@ const estilos = StyleSheet.create({
   },
   texto: {
     marginVertical: 12,
+    fontSize: 18,
   },
 
   nomeApp: {
